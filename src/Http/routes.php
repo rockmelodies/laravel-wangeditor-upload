bf1ec2e37350f71e6011c7ff78de26fc87750686
rocky <939555035@qq.com>
2018-03-18 15:11:21 +0800

    test

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['prefix' => 'laravel-wang-editor'], function () {
 
-    Route::get('example', 'Douyasi\WangEditor\Http\Controllers\WangEditorController@getExample');
-    Route::post('upload', 'Douyasi\WangEditor\Http\Controllers\WangEditorController@postUploadPicture');
+    Route::get('example', 'rockymelody\WangEditor\Http\Controllers\WangEditorController@getExample');
+    Route::post('upload', 'rockymelody\WangEditor\Http\Controllers\WangEditorController@postUploadPicture');
 
 });
